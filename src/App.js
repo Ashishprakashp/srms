540e2b3b576cc46c3153ac5307c3131f73c177d1
Ashishprakashp <apprakash2002@gmail.com>
2025-01-31 09:54:04 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import AdminLogin from './AdminLogin';
+import AdminDashboard from './AdminDashboard';
+
+function App() {
+  return (
+    <AdminDashboard/>
+  );
+}
+
+export default App;
